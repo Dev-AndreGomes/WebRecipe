19ab80d4bd0d10c3df263a457cdce13b5f66f9e8
André Gomes <dev.andregomess@gmail.com>
2024-11-25 02:18:32 -0300

    Adicionando alterações para o catalogo, incluindo pesquisas, categorias, cards, sistema de paginação, e design. Fiz também fazendo o design basico do site como fonte, header, footer e esquemas de cores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,24 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { DetailsComponent } from './details/details.component';
+import { SearchCatalogComponent } from './search-catalog/search-catalog.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  {path:'',redirectTo:'/search', pathMatch:'full'},
+  {path:'search', component: SearchCatalogComponent},
   { path: 'catalog', component: CatalogComponent },
   { path: 'details/:id', component: DetailsComponent },
 ];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class appRoutes{}
